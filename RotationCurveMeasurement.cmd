7409a2c7e610120e23026067af2e7a5b7d1318e1
Eric Gentry <gentry.e@gmail.com>
2012-10-10 21:58:14 -0400

    Initial Commit
    RotationCurveMeasurement.cmd based from "SRT Student Manual" (§2.3.10)

---
+++ RotationCurveMeasurement.cmd
@@ -0,0 +1,22 @@
+: record rotation.rad   (start data recording of file rotation.rad)
+: galactic 206 20    	(unstow and move to calibration position)
+: freq 1420.4 4			
+: noisecal
+: galactic 205 0	 	(measure at galactic lat long)
+: galactic 210 0		(iterate through coords)
+: galactic 215 0
+: galactic 220 0
+: galactic 225 0
+
+: galactic 225 20       (move to calibration position)
+: noisecal
+
+: galactic 230 0
+: galactic 235 0
+: galactic 240 0
+: galactic 245 0
+: galactic 250 0
+
+: stow
+: roff
+
