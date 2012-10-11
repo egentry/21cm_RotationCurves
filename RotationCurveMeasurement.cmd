630b63574c434242067a5a8685e6795eb73ff6fb
Eric Gentry <gentry.e@gmail.com>
2012-10-11 00:58:59 -0400

    Changed Latitude range from >205 to 0<= l <= 90deg
    Changed from looking at longitudes >200deg to between [0,90]degrees.
    Allows for determination of rotational velocities of *inferior*
    rotational geometries (radii smaller than the earth-AGN radius)
    
    Think about a step size smaller than 5degree?

--- RotationCurveMeasurement.cmd
+++ RotationCurveMeasurement.cmd
@@ -2,20 +2,31 @@
 : galactic 206 20    	(unstow and move to calibration position)
 : freq 1420.4 4			
 : noisecal
-: galactic 205 0	 	(measure at galactic lat long)
-: galactic 210 0		(iterate through coords)
-: galactic 215 0
-: galactic 220 0
-: galactic 225 0
+: galactic 0 0	 	(measure at galactic lat long)
+: galactic 5 0		(iterate through coords)
+: galactic 10 0
+: galactic 15 0
+: galactic 25 0
+: galactic 30
 
 : galactic 225 20       (move to calibration position)
 : noisecal
 
-: galactic 230 0
-: galactic 235 0
-: galactic 240 0
-: galactic 245 0
-: galactic 250 0
+: galactic 35 0
+: galactic 40 0
+: galactic 45 0
+: galactic 50 0
+: galactic 60 0
+
+: galactic 225 20
+: noisecal
+
+: galactic 65 0
+: galactic 70 0
+: galactic 75 0
+: galactic 80 0
+: galactic 85 0
+: galactic 90 0
 
 : stow
 : roff
