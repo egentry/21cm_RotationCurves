c25525187a2687c8a916794271e7eb1ec4812f5c
Eric Gentry <gentry.e@gmail.com>
2012-10-17 00:27:20 -0400

    Adding Field-Of-View Scripts
    Replacing the "drift" measurement approach. Now we are manually sweeping
    through 2 directions (azimuth + elevation)
    
    To be updated later: use a npoint scan to find the offset from the
    calculated solar location and actual solar location.
    
    But for now, it gives reasonable data.

---
+++ FOV_azimuth.cmd
@@ -0,0 +1,99 @@
+
+: record azimuth.rad
+: TEST
+: noisecal
+: roff
+
+: Sun
+: record azimuth.rad
+: offset -14 0
+:5
+: roff
+
+
+: Sun
+: record azimuth.rad
+: offset -12 0
+:5
+: roff
+
+
+: Sun
+: record azimuth.rad
+: offset -10 0
+:5
+: roff
+
+: Sun
+: record azimuth.rad
+: offset -8 0
+:5
+: roff
+
+: Sun
+: record azimuth.rad
+: offset -6 0
+:5
+: roff
+
+: Sun
+: record azimuth.rad
+: offset -4 0
+:5
+: roff
+
+: Sun
+: record azimuth.rad
+: offset -2 0
+:5
+: roff
+
+: Sun
+: record azimuth.rad
+: offset -0 0
+:5
+: roff
+
+: Sun
+: record azimuth.rad
+: offset 2 0
+:5
+: roff
+
+: Sun
+: record azimuth.rad
+: offset 4 0
+:5
+: roff
+
+: Sun
+: record azimuth.rad
+: offset 6 0
+:5
+: roff
+
+: Sun
+: record azimuth.rad
+: offset 8 0
+:5
+: roff
+
+: Sun
+: record azimuth.rad
+: offset 10 0
+:5
+: roff
+
+: Sun
+: record azimuth.rad
+: offset 12 0
+:5
+: roff
+
+: Sun
+: record azimuth.rad
+: offset 14 0 
+:5
+: roff
+
+: stow
---
+++ FOV_elevation.cmd
@@ -0,0 +1,99 @@
+
+: record elevation.rad
+: TEST
+: noisecal
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 -14
+:5
+: roff
+
+
+: Sun
+: record elevation.rad
+: offset 0 -12 
+:5
+: roff
+
+
+: Sun
+: record elevation.rad
+: offset 0 -10 
+:5
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 -8 
+:5
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 -6 
+:5
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 -4 
+:5
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 -2 
+:5
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 0
+:5
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 2 
+:5
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 4 
+:5
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 6 
+:5
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 8 
+:5
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 10 
+:5
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 12 
+:5
+: roff
+
+: Sun
+: record elevation.rad
+: offset 0 14
+:5
+: roff
+
+: stow
