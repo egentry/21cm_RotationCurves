be43f2aeed5895418ecb762b4fbd4ba734384bd4
Eric Gentry <gentry.e@gmail.com>
2012-10-22 10:00:42 -0400

    Polished RotationCurveMeasurement
    Changes spectral range to include higher redshifts
    
    Saved each viewing location in its own file.

--- RotationCurveMeasurement.cmd
+++ RotationCurveMeasurement.cmd
@@ -1,52 +1,120 @@
-: record rotation.rad 
+: freq 1419.9 4
 
+: record 10_21_noisecal1.rad
 : TEST 
-: freq 1420.4 4			
 : noisecal
+: roff
 
 : galactic 0 0	
+: record 10_21_G00.rad
 :300
+: roff
+
 : galactic 5 0	
+: record 10_21_G05.rad
 :300
+: roff
+
 : galactic 10 0
+: record 10_21_G10.rad
 :300
+: roff
+
 : galactic 15 0
+: record 10_21_G15.rad
 :300
+: roff
+
+: galactic 20 0
+: record 10_21_G20.rad
+:300
+: roff
+
 : galactic 25 0
+: record 10_21_G25.rad
 :300
+: roff
+
 : galactic 30
+: record 10_21_G30.rad
 :300
+: roff
 
-: TEST    
-: noisecal
-
-: galactic 35 0
-:300
-: galactic 40 0
-:300
-: galactic 45 0
-:300
-: galactic 50 0
-:300
-: galactic 60 0
-:300
-
+: record 10_21_noisecal2.rad
 : TEST 
 : noisecal
+: roff
+
+: galactic 35 0
+: record 10_21_G35.rad
+:300
+: roff
+
+: galactic 40 0
+: record 10_21_G40.rad
+:300
+: roff
+
+: galactic 45 0
+: record 10_21_G45.rad
+:300
+: roff
+
+: galactic 50 0
+: record 10_21_50.rad
+:300
+: roff
+
+: galactic 55 0
+: record 10_21_G55.rad
+:300
+: roff
+
+: galactic 60 0
+: record 10_21_G60.rad
+:300
+: roff
+
+: record 10_21_noisecal3.rad
+: TEST 
+: noisecal
+: roff
 
 : galactic 65 0
+: record 10_21_G65.rad
 :300
+: roff
+
 : galactic 70 0
+: record 10_21_G70.rad
 :300
+: roff
+
 : galactic 75 0
+: record 10_21_G75.rad
 :300
+: roff
+
 : galactic 80 0
+: record 10_21_G80.rad
 :300
+: roff
+
 : galactic 85 0
+: record 10_21_G85.rad
 :300
+: roff
+
+
 : galactic 90 0
+: record 10_21_G90.rad
 :300
+: roff
+
+: record 10_21_noisecal4.rad
+: TEST 
+: noisecal
+: roff
 
 : stow
-: roff
 
